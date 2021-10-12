891fa7f926c9f32e9b1f5574a1414ac1dfd03e7d
justynajanczy <justyna5252@gmail.com>
2021-10-12 23:46:35 +0200

    Initial commit

---
+++ Models/Adult.cs
@@ -0,0 +1,7 @@
+﻿namespace Assignment1.Models
+{
+    public class Adult
+    {
+        
+    }
+}
---
+++ Models/Child.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Assignment1.Models
+{
+    public class Child : Person
+    {
+        public List<Interest> Interests { get; set; }
+        public List<Pet> Pets { get; set; }
+    }
+}
